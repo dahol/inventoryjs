80dec9d58e859927b66ee6a907c148e9ce018e4f
Daniel Holter <daniel.holter@husbanken.no>
2023-05-07 11:25:18 +0200

    link style cleanup

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,19 +4,19 @@ import { Link } from 'react-router-dom'
 // markup
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-light border">
   <div className="container">
-  <Link to="/">Dashboard</Link>
+  <Link className="text-decoration-none" to="/">Dashboard</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item p-2">
-          <Link to="/items">Items</Link>
+          <Link className="text-decoration-none" to="/items">Items</Link>
         </li>
         <li className="nav-item p-2">
-        <Link to="/tasks">Tasks</Link>
+        <Link className="text-decoration-none" to="/tasks">Tasks</Link>
         </li>
       </ul>
       <form className="d-flex">
